1909179635fcbc505b08bf3d0f014c8893d7f6c1
ZigmaSoftware <creative@zigma.in>
2026-06-08 10:24:47 +0530

    recipecreation isuue fixed

--- src/features/recipe-bom-masters/pages/RecipeItemCreationPage.tsx
+++ src/features/recipe-bom-masters/pages/RecipeItemCreationPage.tsx
@@ -93,7 +93,6 @@ const RecipeItemsDialog = ({
 
   useEffect(() => {
     form.reset({ recipe: recipe?.id ?? 0 });
-    setDraftComponents([]);
     setHasUnsavedChanges(false);
     setSearchQuery("");
     setDebouncedSearchQuery("");
@@ -115,13 +114,12 @@ const RecipeItemsDialog = ({
   });
 
   useEffect(() => {
-    if (!detailQ.data) {
-      return;
-    }
-
-    setDraftComponents((detailQ.data.components ?? []).map((component, index) => toDraftBOMComponent(component, index + 1)));
-    setHasUnsavedChanges(false);
-  }, [detailQ.data]);
+    if (!open) return;
+    setDraftComponents(
+      (detailQ.data?.components ?? []).map((component, index) => toDraftBOMComponent(component, index + 1)),
+    );
+    if (detailQ.data) setHasUnsavedChanges(false);
+  }, [detailQ.data, open]);
 
   const subtypeSearchQ = useQuery({
     queryKey: ["recipe-bom-masters", "recipe-item-search", debouncedSearchQuery],
